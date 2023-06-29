f725cabe3ef50e174bdebddbd083c3ff6832d48a
tifff-wang <thiffanyjun@hotmail.com>
2023-06-29 14:05:49 +1200

    Stretch exercise 1,2,3 & 5 complete with restructured code.

--- cafe.js
+++ cafe.js
@@ -2,23 +2,27 @@
 //Products//
 let product = {
   whiteCoffee: {
-    stock: 4,
+    stock: 7,
     price: 4,
+    wholesaleCost: 1,
   },
 
   blackCoffee: {
     stock: 7,
     price: 3.5,
+    wholesaleCost: 0.5,
   },
 
   muffin: {
     stock: 5,
     price: 8.5,
+    wholesaleCost: 3,
   },
 
   eggs: {
     stock: 6,
     price: 12.5,
+    wholesaleCost: 2,
   },
 }
 
@@ -71,6 +75,44 @@ function displayCustomerOrder() {
   console.log(customer.order)
 }
 
+function validateOrder(customerPayment) {
+  /*
+  {
+    'isValidPayment' : true/false,
+    'foodCount': {'egg': 2...},
+    'foodOutOfStok': ['whiteCoffee']
+  }
+  */
+  let result = {}
+
+  let foodCount = {}
+  let totalPrice = 0
+  let foodOutOfStock = []
+
+  customer.order.forEach((item) => {
+    totalPrice += product[item].price
+    if (!foodCount[item]) {
+      foodCount[item] = 1
+    } else {
+      foodCount[item] += 1
+    }
+  })
+  result['foodCount'] = foodCount
+  result['totalPrice'] = totalPrice
+
+  Object.keys(product).forEach((key) => {
+    if (product[key].stock < foodCount[key]) {
+      foodOutOfStock.push(key)
+    }
+  })
+  result['foodOutOfStock'] = foodOutOfStock
+
+  let isValidPayment = customerPayment >= totalPrice
+  result['isValidPayment'] = isValidPayment
+
+  return result
+}
+
 document.getElementById('customerButton').onclick = generateCustomerOrder
 
 //Transaction//
@@ -82,37 +124,75 @@ function displayCash() {
 displayCash()
 
 function fillOrder() {
-  let saleTotal = 0
-  let customerPayment = getRandomInt(5, 50)
-  for (let i = 0; i < customer.order.length; i++) {
-    let productName = customer.order[i]
-    if (product[productName].stock > 0) {
-      product[productName].stock--
-      saleTotal += product[productName].price
-    } else {
-      alert(`I'm sorry, we're out of ${productName}`)
-      //product text to turn red when we run out of a particular product
-      document
-        .getElementById(`${productName}`)
-        .setAttribute('style', 'color:red')
-    }
-  }
+  let customerPayment = getRandomInt(20, 50)
+  let result = validateOrder(customerPayment)
 
-  if (customerPayment >= saleTotal) {
-    cash += saleTotal
+  if (!result.isValidPayment) {
+    let debt = result.totalPrice - customerPayment
+    alert(
+      `Total Charge: $${result.totalPrice} \nCustomer Payment: $${customerPayment} \nSorry, not enough payment, needs to pay $${debt} more.`
+    )
+  } else if (result.foodOutOfStock.length > 0) {
+    const foodStr = result.foodOutOfStock.join(', ')
+    alert(
+      `${foodStr} ${
+        result.foodOutOfStock.length == 1 ? 'is' : 'are'
+      } out of stock`
+    )
+  } else {
+    cash += result.totalPrice
     displayCash()
+
+    Object.keys(result.foodCount).forEach((item) => {
+      product[item].stock -= result.foodCount[item]
+      if (product[item].stock <= 0) {
+        document.getElementById(`${item}`).setAttribute('style', 'color: red')
+      }
+    })
+
     displayProducts()
     customer.order = []
     displayCustomerOrder()
-  } else {
-    let debt = saleTotal - customerPayment
-    alert(
-      `Total Charge: $ ${saleTotal} \nCustomer Payment: $ ${customerPayment} \nSorry, not enough payment, needs to pay ${debt} more.`
-    )
   }
+
+  //
+  //
+  //
 }
 
 document.getElementById('fillOrderButton').onclick = fillOrder
+
+//Wholesale cost and restock
+function addStock(productName) {
+  const food = product[productName]
+  if (cash >= food.wholesaleCost) {
+    console.log(food.stock)
+    food.stock += 1
+    document
+      .getElementById(`${productName}`)
+      .setAttribute('style', 'color:black')
+    displayProducts()
+
+    cash -= food.wholesaleCost
+    displayCash()
+  } else {
+    alert('Out of cash!')
+  }
+}
+
+document.getElementById('restockWhiteCoffee').onclick = function () {
+  addStock('whiteCoffee')
+}
+
+document.getElementById('restockBlackCoffee').onclick = function () {
+  addStock('blackCoffee')
+}
+document.getElementById('restockMuffin').onclick = function () {
+  addStock('muffin')
+}
+document.getElementById('restockEggs').onclick = function () {
+  addStock('eggs')
+}
 
 //Generate a random number//
 function getRandomInt(min, max) {
